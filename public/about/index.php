1f5bb01d720501f88ed949da0a4b12c7324555d7
charlie <charlie@maddex.dev>
2023-12-10 00:38:49 +0000

    Add about and blog pages with initial content

---
+++ public/about/index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" 
+      type="image/png" 
+      href="https://cunjur.uk/favicon.jpg">
+    <link rel="canonical" href="https://cunjur.uk">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- HTML Meta Tags -->
+    <title>cunjur - about</title>
+    <meta name="description" content="cunjur's site">
+
+    <!-- Open Graph (OG) Meta Tags for Facebook and other platforms -->
+    <meta property="og:url" content="https://cunjur.uk/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="cunjur">
+    <meta property="og:description" content="cunjur's site">
+    <meta property="og:image" content="https://cunjur.uk/favicon.jpg">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="cunjur.uk">
+    <meta property="twitter:url" content="https://cunjur.uk/">
+    <meta name="twitter:title" content="cunjur">
+    <meta name="twitter:description" content="cunjur's site">
+    <meta name="twitter:image" content="https://cunjur.uk/favicon.jpg">
+
+    <style>
+        body {
+            font-family: 'Afacad', sans-serif;
+            line-height: 1.4;
+            font-size: 16px;
+            padding: 0 10px;
+            margin: 50px auto;
+            max-width: 650px;
+        }
+
+        ul {
+            margin-top: 0px;
+        }
+
+        #maincontent {
+            max-width: 38em;
+            margin: 15 auto;
+        }
+
+        h2 {
+            margin-bottom: 0px;
+        }
+    </style>
+</head>
+
+<body class="animate__animated animate__fadeIn">
+        <div id="maincontent" style="margin-top:70px">
+        <h2><b>cunjur (<i>charlie</i>)</b></h2><br>
+        <p>
+            <a href="https://cunjur.uk" rel="noopener">Home</a>, <a href="https://cunjur.uk/about" rel="noopener"><b>About</b></a>, <a href="https://cunjur.uk/blog" rel="noopener">Blog</a>, <a href="https://x.com/cunjur" rel="noopener">Twitter</a>, <a href="https://github.com/name" rel="noopener">GitHub</a>
+        </p>
+        <p>
+            I go by cunjur or charlie. I'm currently working with AI and ML to build software systems for clients.
+        </p>
+        <p>
+            Day-to-day I'm working with Python, Rust, and C++ in the cloud and on-premise to build systems that are highly available, scalable, and secure.
+        </p>
+        <small><sup>*</sup> reach out on <a href="https://x.com/cunjur" rel="noopener">Twitter</a>
+    </div>
+</body>
+</html>
---
+++ public/blog/index.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" 
+      type="image/png" 
+      href="https://cunjur.uk/favicon.jpg">
+    <link rel="canonical" href="https://cunjur.uk">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- HTML Meta Tags -->
+    <title>cunjur - about</title>
+    <meta name="description" content="cunjur's site">
+
+    <!-- Open Graph (OG) Meta Tags for Facebook and other platforms -->
+    <meta property="og:url" content="https://cunjur.uk/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="cunjur">
+    <meta property="og:description" content="cunjur's site">
+    <meta property="og:image" content="https://cunjur.uk/favicon.jpg">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="cunjur.uk">
+    <meta property="twitter:url" content="https://cunjur.uk/">
+    <meta name="twitter:title" content="cunjur">
+    <meta name="twitter:description" content="cunjur's site">
+    <meta name="twitter:image" content="https://cunjur.uk/favicon.jpg">
+
+    <style>
+        body {
+            font-family: 'Afacad', sans-serif;
+            line-height: 1.4;
+            font-size: 16px;
+            padding: 0 10px;
+            margin: 50px auto;
+            max-width: 650px;
+        }
+
+        ul {
+            margin-top: 0px;
+        }
+
+        #maincontent {
+            max-width: 38em;
+            margin: 15 auto;
+        }
+
+        h2 {
+            margin-bottom: 0px;
+        }
+    </style>
+</head>
+
+<body class="animate__animated animate__fadeIn">
+        <div id="maincontent" style="margin-top:70px">
+        <h2><b>cunjur (<i>charlie</i>)</b></h2><br>
+        <p>
+            <a href="https://cunjur.uk" rel="noopener">Home</a>, <a href="https://cunjur.uk/about" rel="noopener">About</a>, <a href="https://cunjur.uk/blog" rel="noopener"><b>Blog</b></a>, <a href="https://x.com/cunjur" rel="noopener">Twitter</a>, <a href="https://github.com/name" rel="noopener">GitHub</a>
+        </p>
+        <p>
+            No algo deboosting please.
+        </p>
+        <img src="https://cunjur.uk/cat_heart.jpg" alt="charlie" width="200" height="200"></img>
+        <br><br>
+        <small><sup>*</sup> reach out on <a href="https://x.com/cunjur" rel="noopener">Twitter</a>
+    </div>
+    <script>
+        // Redirect to blog after 3 seconds
+        setTimeout(function() {
+            window.location.href = "https://substack.com/@cunjur";
+        }, 3000);
+    </script>
+</body>
+</html>
