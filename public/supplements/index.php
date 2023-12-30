b20e674d6f8d33d2a4e3a2604085fd0d64830380
charlie <charlie@maddex.dev>
2023-12-30 23:23:05 +0000

    Add supplements page

---
+++ public/supplements/index.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" 
+      type="image/png" 
+      href="https://cunjur.uk/fav.jpg">
+    <link rel="canonical" href="https://cunjur.uk">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- HTML Meta Tags -->
+    <title>cunjur - about</title>
+    <meta name="description" content="cunjur's site">
+
+    <!-- Open Graph (OG) Meta Tags for Facebook and other platforms -->
+    <meta property="og:url" content="https://cunjur.uk/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="cunjur">
+    <meta property="og:description" content="cunjur's site">
+    <meta property="og:image" content="https://cunjur.uk/fav.jpg">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="cunjur.uk">
+    <meta property="twitter:url" content="https://cunjur.uk/">
+    <meta name="twitter:title" content="cunjur">
+    <meta name="twitter:description" content="cunjur's site">
+    <meta name="twitter:image" content="https://cunjur.uk/fav.jpg">
+
+    <style>
+        body {
+            font-family: 'Afacad', sans-serif;
+            line-height: 1.4;
+            font-size: 16px;
+            padding: 0 10px;
+            margin: 50px auto;
+            max-width: 650px;
+        }
+
+        ul {
+            margin-top: 0px;
+        }
+
+        #maincontent {
+            max-width: 38em;
+            margin: 15 auto;
+        }
+
+        h2 {
+            margin-bottom: 0px;
+        }
+
+        #cunjur {
+            background-image: url(https://maddex.dev/anya.png);
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            width: 370px;
+            height: 314px;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
+
+<body class="animate__animated animate__fadeIn">
+        <div id="maincontent" style="margin-top:70px">
+        <h2><b>cunjur (<i>charlie</i>)</b></h2><br>
+        <p>
+            <a href="https://cunjur.uk" rel="noopener">Home</a>, <a href="https://cunjur.uk/about" rel="noopener">About</a>, <a href="https://cunjur.uk/supplements" rel="noopener"><b>Supplements</b></a>, <a href="https://cunjur.uk/blog" rel="noopener">Blog</a>, <a href="https://x.com/cunjur" rel="noopener">Twitter</a>, <a href="https://github.com/name" rel="noopener">GitHub</a>, <a href="https://open.spotify.com/user/kowl7klfi4xxxke4f7t91vtor?si=6a11a5d9b9944b6e" rel="noopener">Spotify</a>
+        </p>
+        <p>
+            <b>Supplements I take <small><sup>inspired by <a href="https://near.blog/supplements/">near's list</a></sup></b>
+            <p style="font-style: italic;">Last updated: 2023-12-30</p>
+            <ul>
+                <li><b>Vitamin C</b> - 1 tablet daily (p/75mg) (£0.02/per day)</li>
+                <li><b>Vitamin D3</b> - 2 tablet daily (p/2000IU) (£0.06/per day)</li>
+                <li><b>Vitamin K2 MK-7</b> - 2 tablet daily (p/100mcg) (£0.06/per day)</li>
+                <li><b>Vitamin K2 MK-4</b> - 2 tablet daily (p/100mcg) (£0.12/per day)</li>
+                <li><b>Vitamin B12</b> - 1 tablet daily (p/5000mcg) (£0.10/per day)</li>
+                <li><b>Caffeine</b> - 1 tablet daily (p/200mg) (£0.03/per day)</li>
+                <li><b>Iron</b> - 1 tablet daily (p/34mg) (£0.07/per day)</li>
+                <li><b>Zinc</b> - 1 tablet daily (p/15mg) (£0.03/per day)</li>
+                <li><b>Copper</b> - 1 tablet daily (p/1mg) (£0.02/per day)</li>
+            </ul>
+            <p><b>Total cost:</b> £0.51/per day, £15.30/per month, and £183.60/per year</p>
+        </p>
+        <small><sup>*</sup> reach out on <a href="https://x.com/cunjur" rel="noopener">Twitter</a>
+    </div>
+
+    <div id="cunjur"></div>
+</body>
+</html>
